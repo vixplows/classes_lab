ff8fbe3dc23c7a6217648047ecacaa381df07abc
vixplows <vicky@vickyplows.co.uk>
2017-05-29 15:49:54 +0100

    Initial commit

---
+++ students/specs/students_spec.rb
@@ -0,0 +1,10 @@
+require('minitest/autorun')
+require("minitest/rg")
+require_relative('../students')
+
+class TestStudents < MiniTest::Test
+  def setup
+  # @bank_account = BankAccount.new("Jarrod", 500, "business")
+  # # @bank_account2 = BankAccount.new("Victoria", 1000, "personal")
+  end
+end
