cce0efee033b60a42eae4b70b25a0d9ee1d86d0d
vixplows <vicky@vickyplows.co.uk>
2017-05-29 22:16:04 +0100

    Pass tests find_player_name

--- team/specs/team_spec.rb
+++ team/specs/team_spec.rb
@@ -26,7 +26,26 @@ class TestTeam < MiniTest::Test
   end
 
   def test_add_player_to_players
-    
+    @team.add_player_to_players("Zany Zee")
+    assert_equal(["Billy Bob", "Junior Joe", "Manic Mary", "Happy Harriet", "Speedy Sam", "Zany Zee"], @team.players)
   end
 
+  def test_find_player_name
+    assert_equal(true, @team.find_player_name("Junior Joe"))
+  end
 end
+
+# Add a method that takes in a string of a player's name and checks to see if they are in the players array.
+
+# def test_find_pet_by_name__returns_pet
+#   pet = find_pet_by_name(@pet_shop, "Arthur")
+#   assert_equal("Arthur", pet[:name])
+# end
+
+# def find_pet_by_name(pet_shop, pet_name)
+#   match = nil
+#   for pet in pets_in_stock(pet_shop)
+#     match = pet if(pet[:name] == pet_name)
+#   end
+#   return match
+# end
--- team/team.rb
+++ team/team.rb
@@ -5,6 +5,27 @@ class Team
     @team_name = team_name
     @players = players
     @coach = coach
-  end 
+  end
 
+  def add_player_to_players(new_player)
+    @players.push(new_player)
+  end
+
+  def find_player_name(name)
+    match = false
+    for player in @players
+      match = true if player == name
+    end
+    return match
+  end
 end
+  
+
+  # end
+
+  # def find_pet_by_name(pet_shop, pet_name)
+   # match = nil
+  #   for pet in pets_in_stock(pet_shop)
+  #     match = pet if(pet[:name] == pet_name)
+  #   end
+  #   return match
